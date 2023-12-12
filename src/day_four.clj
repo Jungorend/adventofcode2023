fc7d9d1dbe5d2571029a3dd0264699f201ca302e
jungy <jungorend@gmail.com>
2023-12-12 15:59:55 -0500

    Part Two done

--- src/day_four.clj
+++ src/day_four.clj
@@ -23,3 +23,26 @@
                          (s/replace "\n" "| ")
                          (s/replace ":" "|")
                          (s/split #"\| ")))))
+
+(defn part-two-calculate-game [current-map [id lucky-numbers actual-numbers]]
+  (let [winning-numbers (reduce #(conj %1 (Integer/parseInt %2)) #{}
+                                (filter seq (s/split lucky-numbers #" ")))
+        numbers (map #(Integer/parseInt %) (filter seq (s/split actual-numbers #" ")))
+        card (Integer/parseInt (last (s/split id #" ")))
+        quantity (inc (or (get current-map card) 0))
+        matching (count (filter #(winning-numbers %) numbers))]
+    (reduce (fn [m v]
+              (assoc m (+ card 1 v) (+ (or (get current-map (+ card 1 v)) 0) quantity)))
+            (assoc current-map card (if (get current-map card)
+                                      (inc (get current-map card))
+                                      1))
+            (range matching))))
+
+(defn part-two [input]
+  (reduce + (vals
+             (reduce part-two-calculate-game {}
+                     (partition 3
+                                (-> input
+                                    (s/replace "\n" "| ")
+                                    (s/replace ":" "|")
+                                    (s/split #"\| ")))))))
